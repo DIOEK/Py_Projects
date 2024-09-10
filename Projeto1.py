72d18f77be8dbc6205291d9757c5763865357be6
Diogo Oliveira <78265065+DIOEK@users.noreply.github.com>
2024-09-10 09:17:45 -0300

    Add files via upload
    test commit

---
+++ Projeto1.py
@@ -0,0 +1,8 @@
+"""Very first Python script: a band name generator that I wrote for an Udemy Course. It does make some cool band names
+sometimes!"""
+
+print("Hello! Welcome to the band name generator!")
+cityName = input("Please, enter  the name of the city you grew up in: ")
+
+petName = input("Please, enter you your current or last pet's name: ")
+print("Your band name could be: ", cityName, petName)
